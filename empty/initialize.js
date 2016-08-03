5badf9adde44a4237458465e86bfa5ea7fa44d8a
Malte W <wessel@rheinschafe.de>
2016-08-03 16:11:47 +0200

    updated noll and added examples

---
+++ empty/initialize.js
@@ -0,0 +1,9 @@
+export default function initialize(canvas, values) {
+    // Initialize logic goes here ...
+    // You have access to
+    //   - the canvas element
+    //   - a `values` object that holds the values of your controls defined in `experiment.json`
+
+    // Return data and objects that you want to access in your `update` function
+    return {};
+}
---
+++ empty/update.js
@@ -0,0 +1,10 @@
+export default function update(canvas, data, values) {
+    // Update and render logic goes here ...
+    // You have access to
+    //   - the canvas element
+    //   - a `data` object that holds objects and data that your `initialize` function returned
+    //   - a `values` object that holds the values of your controls defined in `experiment.json`
+
+    // return false if you want to stop the animation loop
+    return false;
+}
